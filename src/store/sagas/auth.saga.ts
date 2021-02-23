5675274b80222537c9f812826da4b0961d2493c3
Tang Peng <lucky_tangp@163.com>
2021-02-24 07:05:32 +0800

    123

--- src/store/sagas/auth.saga.ts
+++ src/store/sagas/auth.saga.ts
@@ -1,11 +1,10 @@
-import { SIGNUP, SignupAction, signupFail, signupSuccess } from '../actions/auth.action'
+import { SIGNUP, signup, SignupAction, signupFail, signupSuccess } from '../actions/auth.action'
 import { put, takeEvery } from 'redux-saga/effects'
 import axios from 'axios'
 import { API } from '../../config'
 
 function* hanleSignup(action: SignupAction) {
   try {
-    console.log('saga')
     yield axios.post(`${API}/signup`, action.payload)
     yield put(signupSuccess())
   } catch (error) {
